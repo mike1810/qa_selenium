082e41886adb2ccd4b71564471e9a051b0fa039d
mike1018 <mixasikkaliberdiev3@yandex.ru>
2019-07-09 16:47:39 +0400

    It works, but needs to be reworked

--- src/test/java/AutomationPractice.java
+++ src/test/java/AutomationPractice.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
@@ -11,24 +14,87 @@ public class AutomationPractice {
     private static WebDriver driver;
 
     @BeforeClass
-    public static void setup(){
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.com.ua/");
 
-        WebElement search = driver.findElement(By.className("a4blc"));
-        search.sendKeys("GeForce 1050");
-        search.sendKeys(Keys.ENTER);
+
+        //driver.quit();
     }
 
     @Test
-    public void userLogin(){
+    public void userLogin() {
+        driver.get("http://automationpractice.com");
 
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"email_create\"]")).sendKeys("mixasikkaliberdiev@yandex.ru");
+        driver.findElement(By.xpath("//*[@id=\"SubmitCreate\"]/span")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("create_account_error")));
+
+        if (driver.findElement(By.xpath("//*[@id=\"create_account_error\"]")).isDisplayed()) {
+            userLogin2();
+        }
+
+
+        wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//input[@id='id_gender1']")));
+        driver.findElement(By.xpath(".//input[@id='id_gender1']")).click();
+
+        driver.findElement(By.name("customer_firstname")).sendKeys("Mike");
+
+        driver.findElement(By.xpath("//*[@id=\"customer_lastname\"]")).sendKeys("Kaliberdin");
+
+        driver.findElement(By.xpath("//*[@id=\"passwd\"]")).sendKeys("Mike10");
+
+        Select selectDays = new Select(driver.findElement(By.xpath("//*[@id=\"days\"]")));
+        selectDays.selectByValue("10");
+
+        Select selectMonths = new Select(driver.findElement(By.xpath("//*[@id=\"months\"]")));
+        selectMonths.selectByValue("1");
+
+        Select selectYears = new Select(driver.findElement(By.xpath("//*[@id=\"years\"]")));
+        selectYears.selectByValue("1994");
+
+        driver.findElement(By.xpath("//*[@id=\"firstname\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"firstname\"]")).sendKeys("Mike");
+
+        driver.findElement(By.xpath("//*[@id=\"lastname\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"lastname\"]")).sendKeys("Kaliberdin");
+
+        driver.findElement(By.xpath("//*[@id=\"company\"]")).sendKeys("Sperasoft");
+
+        driver.findElement(By.xpath("//*[@id=\"address1\"]")).sendKeys("Donetskaya");
+
+        driver.findElement(By.xpath("//*[@id=\"address2\"]")).sendKeys("666");
+
+        driver.findElement(By.xpath("//*[@id=\"city\"]")).sendKeys("Volgograd");
+
+        Select selectState = new Select(driver.findElement(By.xpath("//*[@id=\"id_state\"]")));
+        selectState.selectByValue("5");
+
+        driver.findElement(By.xpath("//*[@id=\"postcode\"]")).sendKeys("00000");
+        driver.findElement(By.xpath("//*[@id=\"phone\"]")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@id=\"phone_mobile\"]")).sendKeys("88005553535");
+        driver.findElement(By.xpath("//*[@id=\"alias\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"alias\"]")).sendKeys("Donetskaya 666");
+
+
+        driver.findElement(By.xpath("//*[@id=\"submitAccount\"]/span")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"submitAccount\"]/span")));
+        driver.findElement(By.xpath("//*[@id=\"submitAccount\"]/span")).click();
+    }
+
+    public void userLogin2() {
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("mixasikkaliberdiev@yandex.ru");
+        driver.findElement(By.xpath("//*[@id=\"passwd\"]")).sendKeys("Mike10");
+        driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]/span")).click();
     }
 
     @AfterClass
-    public static void tearDown(){
-
+    public static void tearDown() {
+        //driver.quit();
     }
 }
